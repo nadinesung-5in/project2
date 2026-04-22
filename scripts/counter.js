e92212a02b901f088b34c2bbafddd976fb404853
Nadine <nadinehsu.5in@gmail.com>
2026-04-22 15:51:47 +0800

    minus number

--- scripts/counter.js
+++ scripts/counter.js
@@ -1,10 +1,19 @@
 const counter = document.getElementById("counter");
 const number = document.getElementById("counter-button");
+const number2 = document.getElementById("counter-button2");
+
 
 let count = 0;
+
 
 number.addEventListener("click", () => {
     count++;
     counter.textContent = "🩷" + count;
     console.log("like+1");
 });
+
+number2.addEventListener("click",() => {
+    count--;
+    counter.textContent = "🩷" + count;
+    console.log("like-1");
+})
