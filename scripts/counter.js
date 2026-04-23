f8565e07e7a584ebc06d5ce580c89440c2987084
Nadine <nadinehsu.5in@gmail.com>
2026-04-23 15:50:51 +0800

    calculator stage 1

--- scripts/counter.js
+++ scripts/counter.js
@@ -1,6 +1,7 @@
 const counter = document.getElementById("counter");
 const number = document.getElementById("counter-button");
 const number2 = document.getElementById("counter-button2");
+const number3 = document.getElementById("counter-button3");
 
 
 let count = 0;
@@ -16,4 +17,10 @@ number2.addEventListener("click",() => {
     count--;
     counter.textContent = "🩷" + count;
     console.log("like-1");
+})
+
+number3.addEventListener("click",() => {
+    count = 0;
+    counter.textContent = "🩷" + count;
+    console.log("C");
 })
